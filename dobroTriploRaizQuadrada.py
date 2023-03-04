c71a5beed68a768b5c86061dfb90686016a4012f
Aironys1 <aironys1@hotmail.com>
2023-03-04 02:23:54 -0300

    Dobro, Triplo e Raiz Quadrada

---
+++ dobroTriploRaizQuadrada.py
@@ -0,0 +1,12 @@
+# Dobro Triplo Raiz Quadrada
+
+# Crie um algoritimo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+
+numero  = int(input(' Digite um número: '))
+dobro = numero * 2
+triplo = numero * 3
+raizQuadrada = numero ** (1/2)
+
+print('O dobro de {} vale {}.'.format(numero,dobro))
+print('O triplo de {} vale {}.'.format(numero,triplo))
+print('A raiz quadrada de {} é {:.2f}.'.format(numero, raizQuadrada),' 10/3 é: ',  (10/3) )
